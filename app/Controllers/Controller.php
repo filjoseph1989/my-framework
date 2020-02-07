e17f0802154e50c1afdb26efebb175d8b95a83ef
Fil <fil.prospark@gmail.com>
2020-02-08 03:46:50 +0800

    Core: setup a parent controller

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\App;
+
+abstract class Controller
+{
+	/**
+	 * The app instance
+	 * @var object
+	 */
+	private $app;
+
+	/**
+	 * The view instance
+	 * @var object
+	 */
+	private $view;
+
+	/**
+	 * Initiate app instance
+	 *
+	 * @param App $app
+	 */
+	public function __construct(App $app)
+	{
+		$this->app = $app;
+	}
+
+	/**
+	 * Return the app instance
+	 *
+	 * @return object
+	 */
+	protected function app()
+	{
+		return $this->app;
+	}
+
+	/**
+	 * Destruct this controller
+	 */
+	public function __destruct() {}
+}
