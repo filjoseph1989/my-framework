3570c2ebbfdb3f59d33d67138a9d09cde8366510
Fil <fil.prospark@gmail.com>
2020-02-09 01:10:03 +0800

    Core: Initiate sql where condition parsing

--- core/Traits/ObjectMappingTrait.php
+++ core/Traits/ObjectMappingTrait.php
@@ -41,8 +41,54 @@ trait ObjectMappingTrait
      * @param  array  $query
      * @return object
      */
-    public function get(array $query)
+    public function get(array $wheres)
     {
-        return (object) $query;
+        self::prepare($wheres);
+
+        return $this->rows;
+    }
+
+    /**
+     * Prepare the query and get resulting data
+     *
+     * @param  array  $wheres
+     * @return void
+     */
+    private function prepare(array $wheres)
+    {
+        if ($this->database->isConnected()) {
+            $wheres  = self::prepareWhere($wheres);
+            $query   = "select * from {$this->table} where {$wheres};";
+            $results = $this->database->query($query);
+
+            $count = $this->database->count();
+
+            if ($count == -1 || $count == 0) {
+                // Issue 25: redirect to 500 and throw new \Exception("Error Processing Request", 1);
+                echo "Not found exception";
+                return null;
+            }
+
+            foreach ($results as $row) {
+                $this->rows[] = (object) $row;
+            }
+        }
+    }
+
+    /**
+     * Building the where condition
+     *
+     * @param  array  $wheres
+     * @return string
+     */
+    private function prepareWhere(array $wheres)
+    {
+        $whereQuery = [];
+
+        foreach ($wheres as $key => $value) {
+            $whereQuery[] = "$key='$value'";
+        }
+
+        return implode(' AND ', $whereQuery);
     }
 }
