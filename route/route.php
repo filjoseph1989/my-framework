1f44b88b3449d368eb3d476eb7c0a03afdaac152
Fil <fil.prospark@gmail.com>
2020-02-13 02:23:36 +0800

    Core: Created a route.php

--- route/route.php
+++ route/route.php
@@ -2,5 +2,5 @@
 
 # Route definition
 $app->get('/', ['HomeController', 'index']);
-$app->get('/logout', ['LoginController', 'logout']);
+$app->post('/logout', ['LoginController', 'logout']);
 $app->post('/login', ['LoginController', 'login']);
