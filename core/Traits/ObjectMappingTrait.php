c9d1aa6affeb9813b96a9a0c623d92d543aba872
Fil <filjoseph22@gmail.com>
2020-03-05 07:25:26 +0800

    Core: Added update functionality

--- core/Traits/ObjectMappingTrait.php
+++ core/Traits/ObjectMappingTrait.php
@@ -6,9 +6,13 @@ use Core\Model\Database;
 
 /**
  * A trait use by Core\Model\ObjectMapping
+ * 
+ * @author fil beluan <filjoseph22@gmail.com>
  */
 trait ObjectMappingTrait
 {
+    protected int $count = 0;
+
     /**
      * Should return an object map
      *
@@ -36,16 +40,62 @@ trait ObjectMappingTrait
     }
 
     /**
+     * Return the row count of sql query
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return $this->count;
+    }
+
+    /**
      * Return the object result after query
+     * Issue 49
+     * Issue 51
      *
      * @param  array  $query
      * @return object
      */
-    public function get(array $wheres)
+    public function get(array $wheres = [])
     {
-        self::prepare($wheres);
+        self::prepareGet($wheres);
 
         return $this->rows;
+    }
+
+    /**
+     * Update database table
+     * Issue 50
+     *
+     * @param array $data
+     * @return void
+     */
+    public function update(array &$wheres = [], array &$data = [])
+    {
+        return self::prepareUpdate($wheres, $data);
+    }
+
+    /**
+     * Prepare updating table
+     *
+     * @param  array  $wheres
+     * @param  array  $data
+     * @return boolean
+     */
+    private function prepareUpdate(array $wheres, array $data = [])
+    {
+        if ($this->database->isConnected()) {
+            $wheres  = self::prepareWhere($wheres);
+            $query   = self::prepareUpdateQuery($wheres, self::prepareUpdateData($data));
+            $results = $this->database->query($query);
+
+            if (!$results) {
+                return null;
+            }
+
+            return $results;
+        }
     }
 
     /**
@@ -54,16 +104,16 @@ trait ObjectMappingTrait
      * @param  array  $wheres
      * @return void
      */
-    private function prepare(array $wheres)
+    private function prepareGet(array $wheres)
     {
         if ($this->database->isConnected()) {
             $wheres  = self::prepareWhere($wheres);
             $query   = self::prepareSelect($wheres);
             $results = $this->database->query($query);
 
-            $count = $this->database->count();
+            $this->count = $this->database->count();
 
-            if ($count == -1 || $count == 0) {
+            if ($this->count == -1 || $this->count == 0) {
                 return null;
             }
 
@@ -85,6 +135,18 @@ trait ObjectMappingTrait
     }
 
     /**
+     * Prepare update query
+     *
+     * @param string $wheres
+     * @param string $updateData
+     * @return string
+     */
+    private function prepareUpdateQuery(string $wheres, string $updateData)
+    {
+        return "UPDATE {$this->table} SET {$updateData} WHERE {$wheres};";
+    }
+
+    /**
      * Building the where condition
      *
      * @param  array  $wheres
@@ -95,12 +157,40 @@ trait ObjectMappingTrait
         $whereQuery = [];
 
         foreach ($wheres as $key => $value) {
-            $value = trim($value, " \t\n\r\0\x0B");
-            $value = $this->database->scape($value);
-
+            $value = self::scape($value);
             $whereQuery[] = "$key='$value'";
         }
 
         return implode(' AND ', $whereQuery);
+    }
+
+    /**
+     * Prepare update data
+     *
+     * @param  array  $data
+     * @return string
+     */
+    private function prepareUpdateData(array $data)
+    {
+        $updateData = [];
+
+        foreach ($data as $key => $value) {
+            $value = self::scape($value);
+            $updateData[] = "{$key}='{$value}'";
+        }
+
+        return implode(',', $updateData);
+    }
+
+    /**
+     * Scape any value given
+     *
+     * @param mixed $value
+     * @return void
+     */
+    private function scape($value)
+    {
+        $value = trim($value, " \t\n\r\0\x0B");
+        return $this->database->scape($value);
     }
 }
