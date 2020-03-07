b1ee6d795844d8a6638446e6049b5579d736e476
Fil <filjoseph22@gmail.com>
2020-03-07 10:01:14 +0800

    Core: Handle the truthiness of uri againts route

--- core/Router.php
+++ core/Router.php
@@ -90,8 +90,12 @@ class Router
     public function getHandler()
     {
         if ( ! isset($this->routes[$this->uri]) ) {
-            http_response_code(404);
-            return http_response_code();
+            $this->uri = self::lookForPattern();
+
+            if (empty($this->uri)) {
+                http_response_code(404);
+                return http_response_code();
+            }
         }
 
         if ( ! isset($this->routes[$this->uri][$this->requestMethod])) {
@@ -99,5 +103,85 @@ class Router
         }
 
         return $this->routes[$this->uri][$this->requestMethod];
+    }
+
+    /**
+     * Look for true route pattern
+     *
+     * @return string|empty
+     */
+    private function lookForPattern()
+    {
+        $keys = [];
+
+        foreach ($this->routes as $key => $value) {
+            if (count($uriExploded = explode('/', $this->uri)) == count($keyExploded = explode('/', $key))) {
+                $catch = self::matchBaseOnRoute($uriExploded, $keyExploded);
+
+                if ($catch) {
+                    $keys[] = $key;
+                }
+            }
+        }
+
+        return self::truthinessValue($keys, $uriExploded);
+    }
+
+    /**
+     * Match the given uri to the routes
+     *
+     * @param array $uriExploded
+     * @param array $keyExploded
+     * @return string|empty
+     */
+    private function matchBaseOnRoute(array $uriExploded = [], array $keyExploded = [])
+    {
+        for ($i=0; $i < count($keyExploded); $i++) {
+            if (empty($keyExploded) && empty($uriExploded)) {
+                continue;
+            }
+
+            if (preg_match('/{\K[^}]*(?=})/m', $keyExploded[$i], $match)) {
+                $_GET[$match[0]] = $uriExploded[$i];
+                continue;
+            }
+
+            if (($uriExploded[$i] !== $keyExploded[$i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine the truthines of the route catch
+     *
+     * @param array $keys
+     * @param array $uriExploded
+     * @return string|empty
+     */
+    private function truthinessValue(array $keys = [], array $uriExploded = [])
+    {
+        $truthiness      = 0;
+        $truthinessValue = '';
+
+        foreach ($keys as $key => $value) {
+            $valueExploded = explode('/', $value);
+
+            $truth = 0;
+            foreach ($valueExploded as $key2 => $value2) {
+                if ($value2 === $uriExploded[$key2]) {
+                    $truth++;
+                }
+            }
+
+            if ($truth > $truthiness) {
+                $truthiness      = $truth;
+                $truthinessValue = $value;
+            }
+        }
+
+        return $truthinessValue;
     }
 }
