113e05e86280260013016d00d7409f18e22add69
Fil <filjoseph22@gmail.com>
2020-04-07 06:42:21 +0800

    Core: Redefine object mapping contract

--- core/Contracts/ObjectMappingInterface.php
+++ core/Contracts/ObjectMappingInterface.php
@@ -2,7 +2,24 @@
 
 namespace Core\Contracts;
 
+/**
+ * A contract tha protect ObjectMapping
+ */
 interface ObjectMappingInterface
 {
-    public function find(array $arguments);
+    /**
+     * Initiate object mapping
+     *
+     * @param object $object The model object
+     */
+    public function __construct($model);
+
+    /**
+     * Mapping database result
+     *
+     * @param  object $model
+     * @param  array  $columns
+     * @return void
+     */
+    public function map(&$model, $row);
 }
