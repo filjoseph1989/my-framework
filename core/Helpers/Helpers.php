7c055a973605fdaa5f3825c7576a6318042505ad
Fil <filjoseph22@gmail.com>
2020-04-29 11:58:20 +0800

    Core: Remove excerpt and add isJson() function

--- core/Helpers/Helpers.php
+++ core/Helpers/Helpers.php
@@ -3,27 +3,13 @@
 use Core\Iterators\ModelRowIterator;
 
 /**
- * Return sentence(s) of a paragraph
- *
- * @param  string $sub_heading
- * @return string
+ * Determine if string is a json
+ * @var string
  */
-if (!function_exists('excerpt')) {
-    function excerpt(object &$draft, int $sentense = 2)
-    {
-        # Task 60
-        // if (isset($_SESSION['excerpt'][$draft->id]) && !empty($_SESSION['excerpt'][$draft->id])) {
-        //     return $_SESSION['excerpt'][$draft->id];
-        // }
-
-        if (isset($draft->content->sub_heading)) {
-            $expression = "/^([^.!?]*[\.!?]+){0,{$sentense}}/";
-            preg_match($expression, strip_tags($draft->content->sub_heading), $abstract);
-            $_SESSION['excerpt'][$draft->id] = $abstract[0];
-            return $abstract[0] ?? '';
-        }
-
-        return "";
+if (!function_exists('isJson')) {
+    function isJson($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
     }
 }
 
