c2e25fec0980a376600ff5669f99e99515b4775d
Fil <filjoseph22@gmail.com>
2020-10-03 17:25:57 +0800

    updated helpers documentation

--- views/docs/helpers.blade.php
+++ views/docs/helpers.blade.php
@@ -1,10 +1,20 @@
 <div class="bg-gray-100 card p-3 mt-3 rounded shadow">
-    <h2 class="text-2xl">Available helpers:</h2>
+    <h2 class="text-2xl">Available helpers</h2>
     <ul>
         <li class="list-decimal ml-4">
             <article class="method">
+                <h3 class="border-b border-t font-black mt-2 py-1">user()</h3>
+                <pre class="mb-3 mt-1 signature">user()</pre>
+                <p>Return the loggedin user information</p>
+                <h4 class="font-bold mt-2">Parameters</h4>
+                <h4 class="font-bold">Returns</h4>
+                <p>object</p>
+            </article>
+        </li>
+        <li class="list-decimal ml-4">
+            <article class="method">
                 <h3 class="border-b border-t font-black mt-2 py-1">isJson()</h3>
-                <pre class="mb-3 mt-1 signature" style="margin-right: 54px;">isJson(string $string): boolean</pre>
+                <pre class="mb-3 mt-1 signature">isJson(string $string): boolean</pre>
                 <p><em>Check if a string is a valid json string</em></p>
                 <h4 class="font-bold mt-2">Parameters</h4>
                 <table class="ml-5">
@@ -22,7 +32,7 @@
         <li class="list-decimal ml-4">
             <article class="method">
                 <h3 class="border-b border-t font-black mt-2 py-1">isLogin()</h3>
-                <pre class="mb-3 mt-1 signature" style="margin-right: 54px;">isLogin(): boolean</pre>
+                <pre class="mb-3 mt-1 signature">isLogin(): boolean</pre>
                 <p><em>Check if the user has logged in into the system</em></p>
                 <h4 class="font-bold mt-2">Parameters</h4>
                 <h4 class="font-bold">Returns</h4>
@@ -33,7 +43,7 @@
         <li class="list-decimal ml-4">
             <article class="method">
                 <h3 class="border-b border-t font-black mt-2 py-1">asset_version()</h3>
-                <pre class="mb-3 mt-1 signature" style="margin-right: 54px;">asset_version(): boolean</pre>
+                <pre class="mb-3 mt-1 signature">asset_version(): boolean</pre>
                 <p><em>Check if the user has logged in into the system</em></p>
                 <h4 class="font-bold mt-2">Parameters</h4>
                 <h4 class="font-bold">Returns</h4>
