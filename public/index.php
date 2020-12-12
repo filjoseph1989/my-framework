b8718810b9e17bad52cfa6091ea89b3eefc8fa3a
Fil <filjoseph22@gmail.com>
2020-12-12 22:08:51 +0800

    updated namespace

--- public/index.php
+++ public/index.php
@@ -1,14 +1,10 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 define('START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new Core\App;
+$app = new \Core\App;
 
 require __DIR__ . '/../route/route.php';
 
