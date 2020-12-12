51b44890bdfb93b349148ff6172332aa0f128302
Fil <filjoseph22@gmail.com>
2020-12-12 21:56:32 +0800

    just improve the comment

--- core/Request/Request.php
+++ core/Request/Request.php
@@ -65,7 +65,9 @@ class Request implements RequestInterface
     }
 
     /**
-     * Validate request inputs
+     * Here we accept an object $validator
+     * and use $validator to evaluate every input gathered 
+     * by the request
      *
      * @return void
      */
