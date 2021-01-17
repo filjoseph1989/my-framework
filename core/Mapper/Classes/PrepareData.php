b42e36c4210ffa7efcf995a9edbe0993831d5461
Fil <filjoseph22@gmail.com>
2021-01-17 12:36:42 +0800

    added more methods

--- core/Mapper/Classes/PrepareData.php
+++ core/Mapper/Classes/PrepareData.php
@@ -10,22 +10,151 @@ class PrepareData
 
     private array $data;
     private object $database;
-    private string $query;
-    private string $table;
+    private object $model;
     private int $count;
     private int $int;
+    private string $table;
+    private string $query;
+    private string $wherecondition = "";
+    private string $updateCondition = "";
+    private string $order = "";
+    private string $limit = "";
+    private string $offset = ""; # skip
 
-    public function __construct(object $database, string $table, array $data=[])
-    {
-        $this->data = $data;
+    public function __construct(
+        object &$model,
+        object &$database,
+        string $table,
+        array $data=[]
+    ) {
+        $this->data     = $data;
         $this->database = $database;
-        $this->table = $table;
+        $this->table    = $table;
+        $this->model    = $model;
     }
 
     /**
-     * Issue 87
-     * @return
+     * Accept string as sql condition
+     * @param string $condition
      */
+    public function setCondition(string $condition): void
+    {
+        $this->whereCondition = $condition;
+    }
+
+    // Return query where condition string
+    #[PrepareData('getWhereCondition')]
+    public function getWhereCondition(): string
+    {
+        return $this->whereCondition;
+    }
+
+    // Return query update data string
+    #[PrepareData('getUpdateCondition')]
+    public function getUpdateCondition(): string
+    {
+        return $this->updateCondition;
+    }
+
+    // Return query string
+    #[PrepareData('getQueryString')]
+    public function getQueryString(): string
+    {
+        return $this->query;
+    }
+
+    // Prepare select query
+    #[PrepareData('select')]
+    public function select(): void
+    {
+        $this->query = "SELECT * FROM {$this->table}";
+
+        if (!empty($this->whereCondition)) {
+            $this->query = "{$this->query} WHERE {$this->whereCondition}";
+        }
+
+        if (!empty($this->order)) {
+            $this->query = "{$this->query} {$this->order}";
+        }
+
+        if (!empty($this->limit)) {
+            $this->query = "{$this->query} {$this->limit}";
+        }
+
+        if (!empty($this->offset)) {
+            $this->query = "{$this->query} {$this->offset}";
+        }
+    }
+
+    // Prepare skip query
+    #[PrepareData('skip')]
+    public function skip(): void
+    {
+        if ($this->model->limit > 0) {
+            $this->offset = "OFFSET {$this->model->skip}";
+        }
+    }
+
+    // Prepare limit string
+    #[PrepareData('limit')]
+    public function limit(): void
+    {
+        if ($this->model->limit > 0) {
+            $this->limit = "LIMIT {$this->model->limit}";
+        }
+    }
+
+    // Prepare order by string
+    #[PrepareData('orderBy')]
+    public function order(): void
+    {
+        if (strlen($this->model->orderBy) > 0) {
+            $this->order = "ORDER BY {$this->model->orderBy}";
+        }
+    }
+
+    // Building the where condition
+    #[PrepareData('where')]
+    public function where(): void
+    {
+        $whereQuery = [];
+        foreach ($this->model->wheres as $key => $value) {
+            $value        = $this->database->scape($value);
+            $whereQuery[] = "$key='$value'";
+        }
+
+        $this->whereCondition = implode(' AND ', $whereQuery);
+    }
+
+    // Prepare data
+    #[PrepareData('prepareData')]
+    public function updateData()
+    {
+        $values = [];
+
+        foreach ($this->data as $key => $value) {
+            $value = $this->database->scape($value);
+            $values[] = "{$key}='{$value}'";
+        }
+
+        $this->updateCondition = implode(',', $values);
+    }
+
+    /**
+     * Prepare update query
+     * @param  string $updateData
+     */
+    public function update()
+    {
+        $this->query = "UPDATE {$this->table} SET {$this->updateCondition}";
+
+        if ($this->whereCondition != "") {
+            $this->query = "{$this->query} WHERE {$this->whereCondition}";
+        }
+    }
+
+    # Issue 87
+    #[PrepareData('create')]
     public function create()
     {
         if ($this->database->isConnected()) {
@@ -58,12 +187,9 @@ class PrepareData
         }
     }
 
-    /**
-     * Return the inserted ID
-     *
-     * @return int
-     */
-    public function insertedId()
+    // Return the inserted ID
+    #[PrepareData('insertedId')]
+    public function insertedId(): int
     {
         return $this->id;
     }
