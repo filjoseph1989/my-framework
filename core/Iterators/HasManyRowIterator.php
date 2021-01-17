92ea619968c80c0b4466bca4e19237dbf11b8994
fil <fil.elman@ubind.io>
2021-01-17 12:33:59 +0800

    unimplement chained method

--- core/Iterators/HasManyRowIterator.php
+++ core/Iterators/HasManyRowIterator.php
@@ -4,13 +4,8 @@ namespace Core\Iterators;
 
 class HasManyRowIterator implements \Iterator
 {
-    # Containers of rows
     protected array $rows = [];
-
-    # A model passed
     protected object $model;
-
-    # A table a model has many relationship with
     protected object $table;
 
     /**
@@ -23,47 +18,47 @@ class HasManyRowIterator implements \Iterator
         $this->rows  = $model->rows ?? [];
     }
 
-    # Reset index to zero (0)
+    // Reset index to zero (0)
     public function rewind(): object
     {
         return reset($this->rows);
     }
 
-    # Modify values of current index of row
+    // Modify values of current index of row
     public function current(): array
     {
         $hasMany = $this->model->hasMany;
         $rows    = [];
 
         foreach ($hasMany as $key => $many) {
-            $model = (new $many[0])
-                ->where($many[1], current($this->rows)->id)
-                ->get();
-            $rows[$key] = $model;
+            $model = new $many[0];
+            $model->where($many[1], current($this->rows)->id);
+
+            $rows[$key] = $model->get();
         }
 
         return $rows;
     }
 
-    # Return current array index position
+    // Return current array index position
     public function key(): int
     {
         return key($this->rows);
     }
 
-    # Return the next index of rows
+    // Return the next index of rows
     public function next(): object|bool
     {
         return next($this->rows);
     }
 
-    # Return boolean if index exists or not
+    // Return boolean if index exists or not
     public function valid(): bool
     {
         return key($this->rows) !== null;
     }
 
-    # Return the count of rows
+    // Return the count of rows
     public function countRows(): int
     {
         return count($this->rows);
