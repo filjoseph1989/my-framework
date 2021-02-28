5721c7136c542ae845b6d34695cb308f3bd307ca
Fil <filjoseph22@gmail.com>
2021-02-28 23:59:44 +0800

    improve database class

--- core/Model/Database.php
+++ core/Model/Database.php
@@ -2,15 +2,19 @@
 
 namespace Core\Model;
 
+use Core\Traits\DebugTrait;
+
 class Database
 {
+    use DebugTrait;
+
     protected string $Host = '';
     protected string $Port = '';
     protected string $Database = '';
     protected string $User = '';
     protected string $Password = '';
-    protected object $Instance;
     protected string $sql = '';
+    protected object $Instance;
     protected bool $connected = false;
     protected int $rows_count = 0;
 
@@ -30,22 +34,26 @@ class Database
      * Run direct query
      * @param  string $query
      */
+    #[Database('query')]
     public function query(string $query): mixed
     {
         if ($_ENV["DEBUG_QUERY"] == 'true') {
             file_put_contents('query.log', print_r($query, true)."\n", FILE_APPEND);
         }
 
-        $results = $this->Instance->query($this->sql = $query);
-
-        if ($results !== false) {
-            return $results;
+        if ($this->connected === true) {
+            $results = $this->Instance->query($this->sql = $query);
+            if ($results !== false) {
+                return $results;
+            }
         }
+
 
         return null;
     }
 
     // Return the last insert ID
+    #[Database('insertId')]
     public function insertId(): int
     {
         return $this->Instance->insert_id;
@@ -55,12 +63,14 @@ class Database
      * Scape given
      * @param  string $value
      */
+    #[Database('scape')]
     public function scape($value): string
     {
         return $this->Instance->real_escape_string($value);
     }
 
     // Return the count of rows from previous query
+    #[Database('count')]
     public function count(): int
     {
         return $this->Instance->affected_rows;
@@ -70,8 +80,18 @@ class Database
      * Database::connect()
      * Opens up the connection to the database based on the object's attributes¸
      */
+    #[Database('connect')]
     public function connect()
     {
+        if (self::isDatabaseCredentialEmpty()) {
+            file_put_contents(
+                'error.log',
+                print_r('No credentials define in .env for database connection', true) . "\n",
+                FILE_APPEND
+            );
+            return;
+        }
+
         $con = mysqli_connect(
             $this->Host,
             $this->User,
@@ -100,9 +120,8 @@ class Database
     /**
      * Database::unconnect()
      * Closes the database's connection to avoid conflict and release memory
-     *
-     * @return void
      */
+    #[Database('unconnect')]
     public function unconnect()
     {
         if ($this->Instance->close() === false) {
@@ -114,6 +133,7 @@ class Database
      * Return primary key
      * @param  string $table
      */
+    #[Database('getPrimaryKey')]
     public function getPrimaryKey(string $table): string
     {
         if (!$this->connected) return false;
@@ -135,6 +155,7 @@ class Database
      * Return the array of foriegn key(s)
      * @param  string $table
      */
+    #[Database('getForeignKey')]
     public function getForeignKey(string $table): array
     {
         if (!$this->connected) return [];
@@ -160,5 +181,22 @@ class Database
         }
 
         return $return;
+    }
+
+    // Check if there is given credentials on .env
+    #[Database('isDatabaseCredentialEmpty')]
+    private function isDatabaseCredentialEmpty(): bool
+    {
+        if (
+            $this->Host == '' ||
+            $this->User == '' ||
+            $this->Password == '' ||
+            $this->Database == '' ||
+            $this->Port == ''
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
