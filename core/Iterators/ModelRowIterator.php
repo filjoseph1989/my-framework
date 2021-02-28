62fe0cf9cc69590ca7be9e5ec27c98ecea0dd945
Fil <filjoseph22@gmail.com>
2021-02-28 23:55:39 +0800

    changed return to mixed

--- core/Iterators/ModelRowIterator.php
+++ core/Iterators/ModelRowIterator.php
@@ -16,37 +16,37 @@ class ModelRowIterator implements \Iterator
     }
 
     // Reset index to zero (0)
-    public function rewind(): object
+    public function rewind(): mixed
     {
         return reset($this->rows);
     }
 
     // Modify values of current index of row
-    public function current(): object
+    public function current(): mixed
     {
         return current($this->rows);
     }
 
     // Return current array index position
-    public function key(): int
+    public function key(): mixed
     {
         return key($this->rows);
     }
 
     // Return the next index of rows
-    public function next(): bool
+    public function next(): mixed
     {
         return next($this->rows);
     }
 
     // Return boolean if index exists or not
-    public function valid(): bool
+    public function valid(): mixed
     {
         return key($this->rows) !== null;
     }
 
     // Return the count of rows
-    public function countRows(): int
+    public function countRows(): mixed
     {
         return count($this->rows);
     }
