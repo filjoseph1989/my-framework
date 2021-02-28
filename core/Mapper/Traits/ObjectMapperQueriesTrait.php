58093aacfe277b6cf0d87af3ee4b519a2536b383
Fil <filjoseph22@gmail.com>
2021-02-28 23:58:27 +0800

    minimized comments and removed prepareInsertQuery method

--- core/Mapper/Traits/ObjectMapperQueriesTrait.php
+++ core/Mapper/Traits/ObjectMapperQueriesTrait.php
@@ -4,33 +4,14 @@ namespace Core\Mapper\Traits;
 
 use Core\Model\Database;
 
-/**
- * A trait use by Core\Model\ObjectMapper
- *
- * @author fil beluan <filjoseph22@gmail.com>
- */
 trait ObjectMapperQueriesTrait
 {
     /**
      * Prepare delete query
-     *
      * @param  object $model
-     * @return string
      */
-    private function prepareDeleteQuery(object &$model)
+    private function prepareDeleteQuery(object &$model): string
     {
         return "DELETE FROM {$this->table} WHERE `id`={$model->rows[0]->id}";
-    }
-
-    /**
-     * Prepare insert query
-     *
-     * @param string $wheres
-     * @return string
-     */
-    private function prepareInsertQuery(array &$data = [])
-    {
-        $newData = self::prepareInsertData($data);
-        return "INSERT INTO {$this->table} ({$newData['keys']}) VALUES ({$newData['values']})";
     }
 }
