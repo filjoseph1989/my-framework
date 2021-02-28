d1d1eb50b4a1e271c42e28fd4ceeafd48492816b
Fil <filjoseph22@gmail.com>
2021-03-01 00:00:09 +0800

    renamed and removed some methods

--- core/Model/Model.php
+++ core/Model/Model.php
@@ -6,92 +6,78 @@ use Core\Mapper\ObjectMapper;
 use Core\Model\Traits\ModelTrait;
 use Core\Traits\DebugTrait;
 
-/**
- * Main model
- *
- * @author Fil Beluan
- */
 class Model
 {
     use ModelTrait;
     use DebugTrait;
 
+    // The mapping object
+    protected object $mapper;
+    protected array $rows = [];
     public $wasRecentlyCreated = false;
 
-    /**
-     * The mapping object
-     * @var object
-     */
-    protected object $mapper;
-
-    /**
-     * Table rows
-     * @var array
-     */
-    protected array $rows = [];
-
-    /**
-     * Table relationshipe
-     * @var array
-     */
-    protected array $relations = [];
-
-    /**
-     * Initiate model
-     */
     public function __construct()
     {
         $this->mapper = new ObjectMapper($this);
     }
 
-    /**
-     * Destruct this model
-     */
     public function __destruct() {}
+
+    // Return the mapped model
+    public function map($model, $row): object
+    {
+        return $this->mapper->map($model, $row);
+    }
 
     /**
      * Assign value to the given model property
      * @param string $key   The model property
      * @param mixed  $value The property value
      */
-    public function set(string $key, array $value)
-    {
-        $this->$key = $value;
-    }
-
-    /**
-     * Here we set the return rows from database as
-     * models rows
-     *
-     * @param string $key
-     * @param array $value
-     * @return void
-     */
-    public function setModelRows(string $key, array $value)
+    public function setProperty(string $key, mixed $value)
     {
         $this->$key = $value;
     }
 
     /**
      * Remove property from model
-     *
      * @param string $prop  The model property
-     * @return void
      */
     public function unsetProperty($prop)
     {
         unset($this->$prop);
     }
 
-    /**
-     * Return property
-     * @param string $property
-     */
-    private function getProperty(string $property = ''): mixed
+    // Return the array of rows
+    public function getRows(): array
     {
-        if (isset($this->$property)) {
-            return $this->$property;
+        return $this->rows;
+    }
+
+    // Return a count of rows
+    public function countRows(): int
+    {
+        return count($this->rows);
+    }
+
+    // Return the count of hasOne property
+    public function countHasOne(): int
+    {
+        if (!is_null(self::getProperty('hasOne'))) {
+            return count($this->hasOne);
         }
+
+        return 0;
+    }
+
+    // Return the count of hasMany property
+    public function countHasMany(): int
+    {
+        if (!is_null(self::getProperty('hasMany'))) {
+            return count($this->hasMany);
+        }
+
+        return 0;
     }
 
     /**
@@ -117,6 +103,19 @@ class Model
      */
     public function __get($property): mixed
     {
-        return $this->getProperty($property);
+        return self::getProperty($property);
+    }
+
+    /**
+     * Return property
+     * @param string $property
+     */
+    private function getProperty(string $property = ''): mixed
+    {
+        if (isset($this->$property)) {
+            return $this->$property;
+        }
+
+        return null;
     }
 }
